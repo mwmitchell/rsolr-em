03ee233c3d3ad0ca8b8a1b5033fa0c2118883a6f
Matt Mitchell <matt@hotelicopter.com>
2010-10-16 17:55:26 -0400

    re-worked for rsolr master

--- lib/rsolr-em.rb
+++ lib/rsolr-em.rb
@@ -13,23 +13,12 @@ module RSolr
   
   class EM
     
-    include RSolr::Requestable
-    include RSolr::Responseable
-    
-    # override the built-in RSolr send_request method
-    # so we can avoid evaling the response until it's
-    # actually received.
-    def send_and_receive path, opts
-      request_context = build_request path, opts
-      execute request_context
-    end
-    
-    def execute request_context
+    def execute client, request_context
       method = request_context[:method]
       options = {}
       options[:head] = request_context[:headers] if request_context[:headers]
       options[:body] = request_context[:data] if request_context[:body].to_s
-      options[:proxy] ||= self.proxy if self.proxy
+      options[:proxy] ||= request_context[:proxy] if request_context[:proxy]
       
       uri = request_context[:uri]
       http_request = EventMachine::HttpRequest.new(uri.to_s)
@@ -45,7 +34,7 @@ module RSolr
             :status => http.response_header.status,
             :headers => http.response_header
           }
-          solr_response = adapt_response(request_context, response_hash)
+          solr_response = client.adapt_response(request_context, response_hash)
           success_cb_args = case success_cb.arity
           when 1
             [solr_response]
@@ -76,6 +65,7 @@ module RSolr
           error_cb.call request_context, {}, nil
         end
       }
+      nil
     end
     
   end # EMHttp
